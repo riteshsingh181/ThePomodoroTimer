d4ea44248e6c36e14ae18cd2d6c6b951b9c780cb
Ritesh Singh <singhkuritesh1@gmail.com>
2023-08-19 03:29:03 +0530

    Recommitting app.js after reinitializing querySelectors

---
+++ app.js
@@ -0,0 +1,14 @@
+let time = document.querySelector("#time-heading");
+let timeEdit = document.querySelector(".time-edit");
+let timeEditInputs = document.querySelectorAll("input");
+let minutes = document.querySelector("h1#minutes");
+let seconds = document.querySelector("h1#seconds");
+let minutesInp = document.querySelector("input#minutesInp");
+let secondsInp = document.querySelector("input#secondsInp");
+let startBtn = document.querySelector("button#start");
+let started = false;
+
+console.log(minutes);
+console.log(timeEdit);
+console.log(timeEditInputs);
+
